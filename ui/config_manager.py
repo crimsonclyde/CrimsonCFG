56e3c8d4fc86ea6d3b077c4f8acc19e888c3e427
clyde <clyde@example.com>
2025-07-20 19:10:39 +0200

    Fix: Path correction for local.yml

--- ui/config_manager.py
+++ ui/config_manager.py
@@ -53,7 +53,7 @@ class ConfigManager:
                     "git_username": self.get_git_config_value("user.name") or os.environ.get("GIT_USERNAME", getpass.getuser()),
                     "git_email": self.get_git_config_value("user.email") or os.environ.get("GIT_EMAIL", "user@example.com"),
                     "working_directory": "/opt/CrimsonCFG",
-                    "appimg_directory": "/home/{{ system_user }}/AppImages"
+                    "appimg_directory": "/home/" + getpass.getuser() + "/AppImages"
                 }
                 
                 # Render template
